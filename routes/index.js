65639db1b96b25d229c98d6fc79b22790422530f
Armandoajds <129687173+Armandoajds@users.noreply.github.com>
2023-05-13 00:53:41 -0400

    añadiendo bd
    Añadiendo BD

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
 var express = require('express');
+const db = require('../database')
 var router = express.Router();
 
 /* GET home page. */
@@ -6,13 +7,22 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 router.post('/', function(req, res, next)  { 
-  let email = req.body.email;
   let name = req.body.name;
+  let email = req.body.email;
   let coment = req.body.coment;
+  let date = new Date();
+  let ip = req.ip;
   
-  console.log({name, email, coment}) 
+  db.insert(name, email, coment, date,ip);
 
   res.redirect('/')
+});
+
+router.get('/contactos', function(req, res, next) {
+  db.select(function (rows) {
+    console.log(rows);
+  });
+  res.send('En consola');
 });
 
 module.exports = router;
